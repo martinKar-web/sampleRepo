b3850214b590a61e7cab0a16d4b556ce3423e802
Martin Karugaba <martinkarugaba21@gmail.com>
2019-11-03 09:54:49 +0300

    My first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+//
+console.log("Hello world!!!!");
+
+function myFunction() {
+    console.log('This is my function...!');
+    
+}
+//!This is a comment;
+//*This is a comment;
+//?What does this mean?;
+//This is a comment
+//todoThis is a comment;
